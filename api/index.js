da4c374b16c55a1312508deffd082885d4ee9826
adunmoye peter <adunmoyepeter111@gmail.com>
2023-12-23 17:04:27 +0000

    create sign up and api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 })
 
 const app = express();
+app.use(express.json())
 
 
 app.listen(3000, () => {
@@ -19,3 +21,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
